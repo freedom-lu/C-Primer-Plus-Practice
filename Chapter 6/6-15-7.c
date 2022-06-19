4cbf2c18d24b1555424848b3c52ccfd89cb35c7d
freedom-lu <98449455+freedom-lu@users.noreply.github.com>
2022-06-19 21:18:53 +0800

    Add files via upload

---
+++ Chapter 6/6-15-7.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <string.h>
+////编写一个程序把一个单词读入一个字符数组中，然后倒序打印这个单词。
+//void print(char* c, int length)
+//{
+//	for (int i = length; i >= 0; i--)
+//	{
+//		printf("%c", c[i]);
+//	}
+//}
+//int main()
+//{
+//	char c[] = { "abcdefg" };
+//	int length = strlen(c);
+//	print(c, length);
+//	return 0;
+//}
